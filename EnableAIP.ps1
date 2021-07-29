9268018ed8e5a4e38305880d1f7ebfe0f0b217b6
Simon Ozturk <simon@ozturks.net>
2021-07-29 13:29:07 -0400

    Update EnableAIP.ps1

--- EnableAIP.ps1
+++ EnableAIP.ps1
@@ -1,1 +1,35 @@
+using namespace System.Management.Automation
+using namespace System.Net
 
+[ServicePointManager]::SecurityProtocol = [SecurityProtocolType]::Tls12
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned 
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+Install-Module -Name 'MSOnline' -Confirm:$false -Force
+Install-Module -Name 'AzureADPreview' -Confirm:$false -Force
+Install-Module -Name 'MicrosoftTeams' -Confirm:$false -Force
+
+Import-Module -Name 'MSOnline'
+Import-Module -Name 'AzureAD'
+Import-Module -Name 'MicrosoftTeams'
+
+Connect-MsolService
+Connect-AzureAD
+Connect-MicrosoftTeams
+
+
+
+$Template = Get-AzureADDirectorySettingTemplate | Where-Object {$_.DisplayName -eq "Group.Unified"}
+if(!($Setting = Get-AzureADDirectorySetting | Where-Object {$_.TemplateId -eq $Template.Id})) 
+{
+    $Setting = $Template.CreateDirectorySetting()
+}
+$Setting["EnableGroupCreation"] = "False"
+$Setting["GroupCreationAllowedGroupId"] = (Get-AzureADGroup -SearchString "GroupCreators").objectid
+$Setting["EnableMIPLabels"] = "True" 
+$Setting.Values
+
+#New Tenant
+New-AzureADDirectorySetting -DirectorySetting $Setting
+#Existing Tenant
+Set-AzureADDirectorySetting -DirectorySetting $Setting
